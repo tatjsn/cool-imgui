e038f9b360ec741710d93c8a380af1dc4359a7b1
Taz Jackson <tatjsn@users.noreply.github.com>
2020-03-09 23:47:15 +0000

    Clean up more

--- app.cpp
+++ app.cpp
@@ -1,30 +1,30 @@
 #include "imgui.h"
 #include "app.h"
 
-App::App(Db &db):
-db(db)
+App::App(Db &db) : db(db)
 {
   entities = db.getEntities();
 }
 
-void App::render() {
-    ImGui::Begin("App");
+void App::render()
+{
+  ImGui::Begin("App");
 
-    ImGui::BeginChild("scroll", ImVec2(0, ImGui::GetFontSize() * 10));
-    for (auto entity : entities)
-        ImGui::Text("%s %d", entity.name.c_str(), entity.age);
-    ImGui::EndChild();
+  ImGui::BeginChild("scroll", ImVec2(0, ImGui::GetFontSize() * 10));
+  for (auto entity : entities)
+    ImGui::Text("%s %d", entity.name.c_str(), entity.age);
+  ImGui::EndChild();
 
-    static char name[128] = "";
-    ImGui::InputText("Input name", name, IM_ARRAYSIZE(name));
-    static int age;
-    ImGui::InputInt("Input age", &age);
+  static char name[128] = "";
+  ImGui::InputText("Input name", name, IM_ARRAYSIZE(name));
+  static int age;
+  ImGui::InputInt("Input age", &age);
 
-    if (ImGui::Button("Save")) {
-        // TODO make this async
-        Entity entity { 0, name, age };
-        db.addEntity(entity);
-        entities = db.getEntities();
-    }
-    ImGui::End();
+  if (ImGui::Button("Save")) {
+    // TODO make this async
+    Entity entity{0, name, age};
+    db.addEntity(entity);
+    entities = db.getEntities();
+  }
+  ImGui::End();
 }
