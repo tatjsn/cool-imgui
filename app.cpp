7247ba2f44e0fd7530eadaab21ebe1f84478752b
Taz Jackson <tatjsn@users.noreply.github.com>
2020-03-09 23:45:36 +0000

    Clean up

--- app.cpp
+++ app.cpp
@@ -8,16 +8,18 @@ db(db)
 }
 
 void App::render() {
-    ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+    ImGui::Begin("App");
 
     ImGui::BeginChild("scroll", ImVec2(0, ImGui::GetFontSize() * 10));
     for (auto entity : entities)
         ImGui::Text("%s %d", entity.name.c_str(), entity.age);
     ImGui::EndChild();
+
     static char name[128] = "";
     ImGui::InputText("Input name", name, IM_ARRAYSIZE(name));
     static int age;
     ImGui::InputInt("Input age", &age);
+
     if (ImGui::Button("Save")) {
         // TODO make this async
         Entity entity { 0, name, age };
